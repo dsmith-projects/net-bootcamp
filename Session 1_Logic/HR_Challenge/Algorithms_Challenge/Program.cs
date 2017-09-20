79b96a7f3bfe90d3bdaeca8cc89dd76124e32541
dsmith-projects <dss.inbox@gmail.com>
2017-09-20 18:45:34 -0500

    Algorithms Challenge: A Very Big Sum > Solution of the problem

---
+++ Session 1_Logic/HR_Challenge/Algorithms_Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AVeryBigSum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int n = Convert.ToInt32(Console.ReadLine());
+			string[] ar_temp = Console.ReadLine().Split(' ');
+			long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
+			long result = aVeryBigSum(n, ar);
+			Console.WriteLine(result);
+        }
+
+		static long aVeryBigSum(int n, long[] ar)
+		{
+			long sum = 0;
+			for (int i = 0; i < n; i++)
+			{
+				sum += ar[i];
+			}
+			return sum;
+		}
+    }
+}
