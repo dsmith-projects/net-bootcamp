4f64d995a4f6bf2a5bd1f5758ca575bd23aac609
dsmith-projects <dss.inbox@gmail.com>
2017-09-21 15:31:09 -0500

    Cuarto ejercicio en progreso

---
+++ Session 1_Logic/HR_Challenge/MarsExploration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MarsExploration
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            int count = 0;
+            string[] a = new string[S.Length / 3];
+
+            int i = 0;
+            do
+            {
+                string chunk = S.Substring(i, 3);
+                a[i] = chunk;
+                i = i + 3;
+            } while (i < S.Length);
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                //if (a[i])
+                //{
+
+                //}
+            }
+        }
+    }
+}
