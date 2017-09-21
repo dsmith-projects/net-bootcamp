91608739453adbdcd38c65b2f1741cbd4a59c9e4
dsmith-projects <dss.inbox@gmail.com>
2017-09-21 13:13:58 -0500

    En este commit estoy subiendo al repositorio en el servidor los primeros 3 ejercicios y sus respectivas respuestas

---
+++ Session 1_Logic/HR_Challenge/DiagonalDifference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DiagonalDifference
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int n = Convert.ToInt32(Console.ReadLine());
+			int[][] a = new int[n][];
+			for (int a_i = 0; a_i < n; a_i++)
+			{
+				string[] a_temp = Console.ReadLine().Split(' ');
+				a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
+			}
+			int diagonal1 = 0;
+			int diagonal2 = 0;
+			
+            for (int i = n - 1, j = 0; i >= 0; i--, j++)
+			{
+                diagonal1 += a[i][i];
+                diagonal2 += a[i][j];
+			}
+
+            //Console.WriteLine("Diagonal 1: " + diagonal1);
+            //Console.WriteLine("Diagonal 2: " + diagonal2);
+			int resultado = diagonal1 - diagonal2;
+
+			if (resultado < 0)
+			{
+				//Console.WriteLine("Resultado: " + resultado * -1);
+                Console.WriteLine(resultado * -1);
+			}
+			else
+			{
+				//Console.WriteLine("Resultado: " + resultado);
+                Console.WriteLine(resultado);
+			}
+        }
+    }
+}
---
+++ Session 1_Logic/HR_Challenge/GradingStudents/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GradingStudents
+{
+    class MainClass
+    {
+		
+        static int MultiploDe5MasCercano(int n) { // n = 42 (5*8)= 40
+            int multiploDe5 = 1;
+            int i = 0;
+            while(multiploDe5 < n) {
+                multiploDe5 = 5 * i;
+                i++;
+            }
+            //Console.WriteLine("MultiploDe5MasCercano: "+ multiploDe5);
+            return multiploDe5;
+        }
+
+        static int[] Solve(int[] grades)
+		{
+            int multiploDe5MasCercano = 0;
+            int[] result = new int[grades.Length];
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i] < 38)
+                {
+                    result[i] = grades[i];
+                }
+                else
+                {
+                    multiploDe5MasCercano = MultiploDe5MasCercano(grades[i]);
+                    if (multiploDe5MasCercano - grades[i] < 3)
+                    {
+                        result[i] = multiploDe5MasCercano;
+                    }
+                    else
+                    {
+                        result[i] = grades[i];
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] grades = new int[n];
+            for (int grades_i = 0; grades_i < n; grades_i++)
+            {
+                grades[grades_i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] result = Solve(grades);
+            Console.WriteLine(String.Join("\n", result));
+        }
+    }
+}
