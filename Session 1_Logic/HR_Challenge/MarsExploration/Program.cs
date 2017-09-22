961110418bb1361481ee247dad452b5582b5f347
dsmith-projects <dss.inbox@gmail.com>
2017-09-22 11:23:09 -0500

    Resolución del cuarto ejercicio sobre el mensaje de SOS desde Marte

--- Session 1_Logic/HR_Challenge/MarsExploration/Program.cs
+++ Session 1_Logic/HR_Challenge/MarsExploration/Program.cs
@@ -7,25 +7,33 @@ namespace MarsExploration
         public static void Main(string[] args)
         {
             string S = Console.ReadLine();
-
-            int count = 0;
             string[] a = new string[S.Length / 3];
-
+            int corruptedLetters = 0;
+            int count = 0;
             int i = 0;
             do
             {
                 string chunk = S.Substring(i, 3);
-                a[i] = chunk;
+                a[count++] = chunk;
                 i = i + 3;
             } while (i < S.Length);
 
-            for (int i = 0; i < a.Length; i++)
+            for (int j = 0; j < a.Length; j++)
             {
-                //if (a[i])
-                //{
-
-                //}
+                if (a[j][0] != 'S')
+                {
+                    corruptedLetters++;
+                }
+				if (a[j][1] != 'O')
+				{
+					corruptedLetters++;
+				}
+				if (a[j][2] != 'S')
+				{
+					corruptedLetters++;
+				}
             }
+            Console.WriteLine(corruptedLetters);
         }
     }
 }
