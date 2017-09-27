9dbdf6d9eec5c8b4c3cde40ef552e82d33b1fb1e
dsmith-projects <dss.inbox@gmail.com>
2017-09-27 08:24:20 -0600

    This is the first draft of the application. From this code I will create a branch or branches and work on them

--- Session 1_Logic/InventoryAppChallenge/InventoryApp/Program.cs
+++ Session 1_Logic/InventoryAppChallenge/InventoryApp/Program.cs
@@ -6,7 +6,25 @@ namespace InventoryApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			if (args.Length == 1)
+			{
+				if (args[1].ToLower() == "admin")
+				{
+					Console.WriteLine("Run app as admin");
+				}
+				else
+				{
+					Console.WriteLine("Please try again! Make sure you start the app as a user or as the admin");
+				}
+			}
+			else if (args.Length == 0)
+			{
+				Console.WriteLine("Run app as user");
+			}
+			else
+			{
+				Console.WriteLine("Please try again! Make sure you start the app as a user or as the admin");
+			}
         }
     }
 }
