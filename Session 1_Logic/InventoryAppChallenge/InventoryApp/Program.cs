47fea1ddc8f975413f4f3b300b897e884268724b
dsmith-projects <dss.inbox@gmail.com>
2017-09-27 12:13:57 -0600

    Application start up settings. Programming conditional to decide whether user is admin or not

--- Session 1_Logic/InventoryAppChallenge/InventoryApp/Program.cs
+++ Session 1_Logic/InventoryAppChallenge/InventoryApp/Program.cs
@@ -6,11 +6,14 @@ namespace InventoryApp
     {
         public static void Main(string[] args)
         {
-			if (args.Length == 1)
+			Console.WriteLine("Number of command line parameters = {0}", args.Length);
+
+            if (args.Length == 1)
 			{
-				if (args[1].ToLower() == "admin")
+				if (args[0].ToLower() == "admin")
 				{
 					Console.WriteLine("Run app as admin");
+                    bool authenticated = AuthenticateAdminUser();
 				}
 				else
 				{
@@ -25,6 +28,47 @@ namespace InventoryApp
 			{
 				Console.WriteLine("Please try again! Make sure you start the app as a user or as the admin");
 			}
+        }
+
+        public static bool AuthenticateAdminUser() 
+        {
+            string username = "";
+            string password = "";
+			const string DefaultUsername = "hola";
+			const string DefaultPassword = "mundo";
+
+            const int MaxNumAttempts = 3;
+            int numAttempt = 0;
+            bool userVerified = false;
+            bool authenticated = false;
+
+            while (!userVerified || numAttempt < MaxNumAttempts)
+            {
+                Console.Write("Please enter your username: ");
+                username = Console.ReadLine();
+                Console.Write("Please enter your password: ");
+                password = Console.ReadLine();
+
+                numAttempt++;
+
+                if (username.Equals(DefaultUsername) && password.Equals(DefaultPassword))
+                {
+                    userVerified = true;
+                    authenticated = true;
+                } 
+                else
+                {
+                    Console.WriteLine("Try again! Either the username or the password is incorrect. {0} attemps left", (MaxNumAttempts - numAttempt));
+                }
+
+            }
+
+            if(numAttempt >= MaxNumAttempts) {
+                Console.WriteLine("Locking the account >> TODO");
+            }
+
+            return authenticated;
+
         }
     }
 }
