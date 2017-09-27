19016e88baec401b8c3e456087fa57d05b846ea1
dsmith-projects <dss.inbox@gmail.com>
2017-09-27 14:57:52 -0600

    Created the admin module a authentication method

--- Session 1_Logic/InventoryAppChallenge/InventoryApp/Program.cs
+++ Session 1_Logic/InventoryAppChallenge/InventoryApp/Program.cs
@@ -12,9 +12,17 @@ namespace InventoryApp
 			{
 				if (args[0].ToLower() == "admin")
 				{
-					Console.WriteLine("Run app as admin");
+					//Console.WriteLine("Run app as admin");
                     bool authenticated = AuthenticateAdminUser();
-				}
+                    Console.WriteLine("authenticated: {0}", authenticated);
+                    if (authenticated)
+                    {
+                        RunAdminModule();
+                    } else
+                    {
+                        Console.WriteLine("Access denied. Try again or contact your system administrator");
+                    }
+                }
 				else
 				{
 					Console.WriteLine("Please try again! Make sure you start the app as a user or as the admin");
@@ -42,7 +50,7 @@ namespace InventoryApp
             bool userVerified = false;
             bool authenticated = false;
 
-            while (!userVerified || numAttempt < MaxNumAttempts)
+            do
             {
                 Console.Write("Please enter your username: ");
                 username = Console.ReadLine();
@@ -50,25 +58,51 @@ namespace InventoryApp
                 password = Console.ReadLine();
 
                 numAttempt++;
-
+                //Console.WriteLine("Username: {0} - Password: {1}", username, password);
+                //Console.WriteLine("DefaultUsername: {0} - DefaultPassword: {1}", DefaultUsername, DefaultPassword);
                 if (username.Equals(DefaultUsername) && password.Equals(DefaultPassword))
                 {
                     userVerified = true;
                     authenticated = true;
-                } 
-                else
+                }
+                else if(numAttempt < 3)
                 {
                     Console.WriteLine("Try again! Either the username or the password is incorrect. {0} attemps left", (MaxNumAttempts - numAttempt));
                 }
 
-            }
+            } while (!userVerified && numAttempt < MaxNumAttempts);
 
             if(numAttempt >= MaxNumAttempts) {
-                Console.WriteLine("Locking the account >> TODO");
+                Console.WriteLine("Maximum number of attempts reached\nLocking your account. Contact your system administrator >> TODO");
             }
 
             return authenticated;
-
         }
+
+        public static void RunAdminModule()
+        {
+            int option = DisplayMenu();
+
+            switch (option)
+            {
+                case 1:
+                    break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+                default:
+                    break;
+            }
+        }
+
+        public static int DisplayMenu() {
+            
+        }
+
     }
 }
