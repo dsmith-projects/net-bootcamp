e4b3485457c4cf8ff6af2bebcd9f3410275f39ef
dsmith-projects <dss.inbox@gmail.com>
2017-11-14 16:10:23 -0800

    Added three classes. One for each of the entities that i will be working with

---
+++ Session 3_Data/InventoryAppWithDB/InventoryAppWithDB/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAppWithDB
+{
+    public class Customer
+    {
+        public string Customer_id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Session 3_Data/InventoryAppWithDB/InventoryAppWithDB/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAppWithDB
+{
+    public class Invoice
+    {
+        public string Customer_id { get; set; }
+        public int Product_id { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Purchase_date { get; set; }
+    }
+}
